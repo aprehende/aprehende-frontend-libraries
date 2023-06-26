a8164b467afc1df83f5b5a355a013921ff95dce2
Jordan Jaramillo <vednadroj@gmail.com>
2023-06-26 16:56:37 -0500

    feat: test for the chip component

---
+++ packages/aprehende-ui/src/Chip/test/Chip.test.tsx
@@ -0,0 +1,29 @@
+import { render } from '@testing-library/react';
+import { describe, it, vi } from 'vitest';
+
+import Chip from '../Chip';
+
+describe('Chip', () => {
+  it('should match the snapshot', () => {
+    const { container } = render(<Chip>Chip</Chip>);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render the chip with the text', () => {
+    const { getByText } = render(<Chip>Chip</Chip>);
+    expect(getByText('Chip')).toBeInTheDocument();
+  });
+
+  it('should render close icon', () => {
+    const fn = vi.fn();
+    const { getByAltText } = render(<Chip onDelete={fn}>Chip</Chip>);
+    expect(getByAltText('close')).toBeInTheDocument();
+  });
+
+  it('should execute the onDelete function', () => {
+    const fn = vi.fn();
+    const { getByAltText } = render(<Chip onDelete={fn}>Chip</Chip>);
+    getByAltText('close').click();
+    expect(fn).toHaveBeenCalled();
+  });
+});
